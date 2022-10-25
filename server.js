c6c091b21a22e0eac094db13bd9cebf33b48508b
Rodrigo de Leon <rodri230998@gmail.com>
2022-10-24 19:09:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,171 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var bodyParser = require('body-parser');
+var express = require('express'); // Express web server framework
+var request = require('request'); // "Request" library
+var cors = require('cors');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+var client_id =  "66098351d90847cb967ed4105909344e"// Your client id
+var client_secret = "d607c8eb0e8346d7b5e584f65fbb39f1"  // Your secret
+var redirect_uri = "https://my-palnorte.herokuapp.com/callback"// Your redirect uri
+var meu_token = "" // Your token
 
-app.use(express.static('public'))
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html')
-})
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
 
-app.get('/callback', (req, res) => {
-  res.sendFile(__dirname + '/public/download.html')
-})
+var stateKey = 'spotify_auth_state';
 
-app.get('/login', (req, res) => {
-  res.redirect('https://accounts.spotify.com/en/authorize?response_type=code&client_id=66098351d90847cb967ed4105909344e&scope=user-read-private%20user-read-email%20user-top-read%20user-follow-read&redirect_uri=https%3A%2F%2Fmy-palnorte.herokuapp.com%2Fcallback&state=sSD1FoXaEZZYxRwd')
-})
+var app = express();
+var engines = require('consolidate');
 
-app.listen(process.env.PORT || port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.set('views', __dirname + '/public');
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html');
+
+app.use(express.static(__dirname))
+   .use(cors())
+   .use(cookieParser());
+
+app.get('/login', function(req, res) {
+
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email user-top-read user-follow-read';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+
+
+app.get('/callback', function(req, res) {
+
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    // requesting access token from refresh token
+    var refresh_token = req.query.refresh_token;
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+      form: {
+        grant_type: 'refresh_token',
+        refresh_token: refresh_token
+      },
+      json: true
+    };
+  
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        meu_token = body.access_token;
+        res.send({
+          'access_token': meu_token
+        });
+      }
+    });
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+      res.clearCookie(stateKey);
+      var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+          code: code,
+          redirect_uri: redirect_uri,
+          grant_type: 'authorization_code'
+        },
+        headers: {
+          'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+        },
+        json: true
+      };
+      request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var access_token = body.access_token, 
+            refresh_token = body.refresh_token;;
+        
+        var options = {
+          url: 'https://api.spotify.com/v1/me/top/artists?limit=18',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          var artistas = [];
+          var imgURL = [];
+          let contador = 0;
+          var tamanho = body.items.length;
+
+          for (var i = 0; i < body.items.length; i++) {
+            artistas[i] = body.items[i].name;
+          }
+
+          res.render(__dirname + '/resultado2.html', {artistas: artistas, tamanho: tamanho});
+          
+          });
+
+      } else{
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
+
+app.get('/refresh_token', function(req, res) {
+
+  // requesting access token from refresh token
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      });
+    }
+  });
+});
+
+function enviar(res, artistas){
+  res.render(__dirname + '/resultado2.html', {artistas: artistas});
+}
+app.listen(process.env.PORT || 8888, function () {
+  console.log('listening');
+});
